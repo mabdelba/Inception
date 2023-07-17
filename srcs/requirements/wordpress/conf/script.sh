f0f8d2fad0d00023637efabb43fa3707bc4b8c28
abdelbar <abdelbar.mohamed1998@gmail.com>
2023-07-17 02:30:31 -0400

    update: php-fpm

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -8,4 +8,12 @@ wp core download --allow-root
 
 wp core config --dbhost=mariadb-container  --dbname=wordpress --dbuser=mabdelba --dbpass=0000 --allow-root
 
-sleep 10000
+sed -i "s/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g" /etc/php/7.4/fpm/pool.d/www.conf
+
+php-fpm7.4 -F
+
+# sleep 10000
+
+# php-fpm7.4 -F
+#/etc/php/7.4/fpm/pool.d
+# listen = /run/php/php7.4-fpm.sock
