1b4abf5aeba2923b3619deb34dcf93004ca29fb2
abdelbar <abdelbar.mohamed1998@gmail.com>
2023-07-20 05:21:25 -0400

    fix: root password

--- srcs/requirements/mariadb/conf/file.sh
+++ srcs/requirements/mariadb/conf/file.sh
@@ -5,5 +5,7 @@ sed -i 's/127.0.0.1/0.0.0.0/g' etc/mysql/mariadb.conf.d/50-server.cnf
 echo "create user '$MYSQL_USER'@'%' identified by '$MYSQL_PASSWORD';" | mariadb
 echo "create database $MYSQL_DB;" | mariadb
 echo "grant all privileges on $MYSQL_DB.* to $MYSQL_USER;" | mariadb
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mariadb
+kill `cat /var/run/mysqld/mysqld.pid`
 service mariadb stop
 mysqld
--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -11,7 +11,9 @@ wp core download --allow-root
 
 wp core config --dbhost=mariadb  --dbname=wordpress --dbuser=mabdelba --dbpass=0000 --allow-root
 
-wp core install --url=mabdelba.42.fr --title=inception --admin_user=mohamed --admin_password=0000 --admin_email=abdelbar.mohammed19@gmail.com --allow-root
+wp core install --url=$DOMAIN_NAME --title=inception --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PW --admin_email=$WP_ADMIN_EMAIL --allow-root
+
+wp user create $WP_USER $WP_EMAIL --user_pass=$WP_PW --role=subscriber --allow-root
 fi
 ###
 sed -i "s/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g" /etc/php/7.4/fpm/pool.d/www.conf
